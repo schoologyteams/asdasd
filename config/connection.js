d9b820d7acbd6203a74aae00453c6694ee729f1c
zhagan <zhagan@gmail.com>
2018-02-25 20:12:21 -0700

    login update

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,14 @@
 const mysql = require('mysql');
 var connection = {};
 
-if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(
-    process.env.JAWSDB_URL
+    host: localhost,
+    port: 8889,
+    user: root,
+    password: root,
+    database: 'burgers_db'
   );
 
-} else {
-  connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: 'burgers_db'
-  });
-}
 
 connection.connect(function(err) {
   if (err) throw err;

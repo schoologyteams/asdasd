695cda3ebfe17f2ae970695a4b7f938c93df6029
zhagan <zhagan@gmail.com>
2018-02-25 20:20:54 -0700

    ports

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,13 @@
 const mysql = require('mysql');
-var connection = {};
 
-  connection = mysql.createConnection(
-    host: localhost,
-    port: 8889,
-    user: root,
-    password: root,
-    database: 'burgers_db'
-  );
 
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  port: 8889,
+  database: "burgers_db"
+});
 
 connection.connect(function(err) {
   if (err) throw err;

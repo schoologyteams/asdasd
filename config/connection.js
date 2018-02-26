f9c2fc0884db06ef1e88014a1b4626bbdfb945fb
zhagan <zhagan@gmail.com>
2018-02-25 21:38:16 -0700

    aws

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  port: 8889,
-  database: "burgers_db"
+  host: "ec2-174-129-33-29.compute-1.amazonaws.com",
+  user: "jfaranbuybthxj",
+  password: "9984cc0dbd7ad4e38ced25345a171a5f6291117053bbf4ef079c50cfcd326449",
+  port: 5432,
+  database: "de0gqukbr0hu7u"
 });
 
 connection.connect(function(err) {
